1343fb34d5ab5470fd9ba84a52b3de451dc30475
João Victor Pires Dos Santos <54391360+Jv-Pi@users.noreply.github.com>
2023-01-20 12:43:52 -0400

    criado a estrutura geral do projeto e incrementado o banco de dados MYSQL

--- Index.js
+++ Index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Banco',
+    database: 'Principal'
+});
+connection.connect(function(err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+    console.log('connected to my server');
+})
+
 app.set('view engine', 'ejs')
-app.use(express.static(Portfolio + '/views/css'));
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/Js'));
 app.get('/', (req, res) =>
     res.render('index')
 )
